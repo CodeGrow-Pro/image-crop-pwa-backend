35bc1069f4e601cbb340a4c28a277818f06b9069
CodeGrow-Pro <vk4993102@gmail.com>
2024-03-07 21:19:45 +0530

    first commit

--- config/database.js
+++ config/database.js
@@ -1,8 +1,22 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "Vishal@123",
+//     DB: "cropImageDB",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Vishal@123",
-    DB: "cropImageDB",
+    HOST: "sql6.freesqldatabase.com",
+    USER: "sql6689359",
+    PASSWORD: "Z4IWZPcBYl",
+    DB: "sql6689359",
     dialect: "mysql",
     pool: {
         max: 5,
